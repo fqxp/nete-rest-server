ec60d0ed1ec1f4f5e33adf53fe238bd30d76184d
Frank Ploss <frank@fqxp.de>
2012-04-28 15:01:36 +0200

    Interpret '_method' HTTP parameter as HTTP method

--- nete/rest/handlers/base.py
+++ nete/rest/handlers/base.py
@@ -6,19 +6,23 @@ import tornado.web
 logger = logging.getLogger(__name__)
 
 class BaseApiHandler(tornado.web.RequestHandler):
+    METHOD_PARAM = '_method'
+
     def initialize(self):
+        if self.request.method == 'GET' and self.request.arguments.has_key(self.METHOD_PARAM):
+            self.request.method = self.request.arguments[self.METHOD_PARAM][0].upper()
+            del self.request.arguments[self.METHOD_PARAM]
+
         self.set_header('Content-Type', 'application/json')
         self.nete_db = self.settings[u'nete_db']
 
     def write_error(self, status_code, **kwargs):
-        exception = kwargs.get('exc_info')
+        exception = kwargs.get('exc_info')[1]
 
         error_doc = {u'success': False}
-        if hasattr(exception, u'log_message'):
-            error_doc[u'message'] = exception.log_message
+        if hasattr(exception, u'message'):
+            error_doc[u'message'] = exception.message
         else:
             error_doc[u'message'] = httplib.responses[status_code]
-
-        error_doc.update(kwargs)
 
         self.finish(json.dumps(error_doc, skipkeys=True))
